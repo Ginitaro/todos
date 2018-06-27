ba0a1cfbb1cfd7af5c64d994a38b32a7e812c26e
Ruben de Haas <ruben.dehaas@activegamingmedia.com>
2018-06-27 16:38:50 +0900

    set comments

--- test.go
+++ test.go
@@ -32,18 +32,22 @@ func index_handler(w http.ResponseWriter, r *http.Request) {
 	var todo_data []TodoData
 
 	db.View(func(tx *bolt.Tx) error {
-		// Assume bucket exists and has keys
+
+		// Get TodoBucket instance
 		b := tx.Bucket([]byte("TodoBucket"))
 		c := b.Cursor()
 
+		// Get all data in TodoBucket
 		for k, v := c.First(); k != nil; k, v = c.Next() {
 
 			var todo_data_item TodoData
 
+			// Unseriale each row in TodoBucket
 			if err := json.Unmarshal(v, &todo_data_item); err != nil {
 				panic(err)
 			}
 
+			// Append unserialized TodoData into array
 			todo_data = append(todo_data, todo_data_item)
 		}
 
@@ -61,27 +65,40 @@ func itob(v int) []byte {
 }
 
 func tododata_create_handler(w http.ResponseWriter, r *http.Request) {
+
+	// Load template
 	tmpl := template.Must(template.ParseFiles("tododata_create.html"))
+
+	// Check if request is POST
 	if r.Method != http.MethodPost {
+
+		// Render view
 		tmpl.Execute(w, nil)
 		return
 	} else {
 
+		// Create instance of TodoData based on form input
 		todo_data := TodoData{
 			Title: r.FormValue("title"),
 		}
 
+		// Handle DB changes
 		db.Update(func(tx *bolt.Tx) error {
+
+			// Get TodoBucket instance
 			b := tx.Bucket([]byte("TodoBucket"))
 
+			// Find next available id for autoincrementing
 			id, _ := b.NextSequence()
 			todo_data.ID = int(id)
 
+			// Serialize TodoData instance
 			buf, err := json.Marshal(todo_data)
 			if err != nil {
 				return err
 			}
 
+			// Update TodoData record in DB
 			return b.Put(itob(todo_data.ID), buf)
 		})
 
@@ -90,42 +107,59 @@ func tododata_create_handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func todo_create_handler(w http.ResponseWriter, r *http.Request) {
+
+	// Load template
 	tmpl := template.Must(template.ParseFiles("todo_create.html"))
+
+	// Check if request is POST
 	if r.Method != http.MethodPost {
+
+		// Render view
 		tmpl.Execute(w, nil)
 		return
 	} else {
 
+		// Create instance of Todo based on form input
 		todo := Todo{
 			Name: r.FormValue("name"),
 		}
 
-		tododata_id, err1 := strconv.Atoi(mux.Vars(r)["id"])
-
-		if err1 != nil {
+		// Convert id<string> from request to id<int>
+		tododata_id, err := strconv.Atoi(mux.Vars(r)["id"])
+		if err != nil {
 			log.Fatal(tododata_id)
 		}
 
+		// Handle DB changes
 		db.Update(func(tx *bolt.Tx) error {
+
+			// Get TodoBucket instance
 			b := tx.Bucket([]byte("TodoBucket"))
+
+			// Get TodoData JSON by id from TodoBucket
 			v := b.Get([]byte(itob(tododata_id)))
 
 			var todo_data_item TodoData
 
+			// Unserialize JSON to Tododata instance
 			if err := json.Unmarshal(v, &todo_data_item); err != nil {
 				panic(err)
 			}
 
+			// Find next available id for autoincrementing
 			id, _ := b.NextSequence()
 			todo.ID = int(id)
 
+			// Append Todo into Todos of the TodoData instance
 			todo_data_item.Todos = append(todo_data_item.Todos, todo)
 
+			// Serialize TodoData instance
 			buf, err := json.Marshal(todo_data_item)
 			if err != nil {
 				return err
 			}
 
+			// Update TodoData record in DB
 			return b.Put(itob(todo_data_item.ID), buf)
 		})
 
@@ -133,7 +167,10 @@ func todo_create_handler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Core function
 func main() {
+
+	// Open the DB connection
 	var err error
 	db, err = bolt.Open("my.db", 0600, nil)
 	if err != nil {
@@ -141,6 +178,7 @@ func main() {
 	}
 	defer db.Close()
 
+	// Initialize TodoBucket bucket
 	db.Update(func(tx *bolt.Tx) error {
 		b, err := tx.CreateBucket([]byte("TodoBucket"))
 		if err != nil {
@@ -150,6 +188,7 @@ func main() {
 		return nil
 	})
 
+	// Dev, print all data from DB in TodoBucket
 	db.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte("TodoBucket"))
 		v := b.Get([]byte(itob(1)))
@@ -157,6 +196,7 @@ func main() {
 		return nil
 	})
 
+	// Initialize Router
 	r := mux.NewRouter()
 	r.HandleFunc("/", index_handler).Methods("GET")
 	r.HandleFunc("/create", tododata_create_handler).Methods("GET", "POST")
